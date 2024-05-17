3958260051b3531ded0b2c45d795b38b0058456f
Luciano <lucianoalejandromas@gmail.com>
2024-05-17 02:58:11 -0300

    railway relation

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,8 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const sequelize_1 = require("sequelize");
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new sequelize_1.Sequelize(process.env.DB_URL, {
     dialect: "postgres",
     logging: false
 });
