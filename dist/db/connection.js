101f9ef15509c849308448c2118550222f363906
Luciano <lucianoalejandromas@gmail.com>
2024-05-17 04:30:22 -0300

    railway relation

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,7 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const sequelize_1 = require("sequelize");
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize(process.env.DB_URL, {
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "postgres",
     logging: false
 });
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,9 +3,15 @@ import { Sequelize } from "sequelize"
 
 dotenv.config()
 
-const sequelize = new Sequelize(process.env.DB_URL!, {
-  dialect: "postgres",
-  logging: false
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false
+  }
+)
 
 export default sequelize
