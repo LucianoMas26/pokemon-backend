3f47a0902f91c4dd20e73f6581adfcdf1128845a
Luciano <lucianoalejandromas@gmail.com>
2024-06-25 22:00:56 -0300

    ajustando conexiones para render

--- dist/config.js
+++ dist/config.js
@@ -9,6 +9,12 @@ dotenv_1.default.config();
 const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectedUnauthorized: false
+        }
+    },
     logging: false
 });
 exports.default = sequelize;
